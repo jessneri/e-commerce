1b3d46127ab0c4e160a4fe82fb9c698a7734ca82
Jessica Neri <jess.neri94@gmail.com>
2021-01-13 14:45:30 -0300

    Doc: main layout(material ui) and commercejs api done.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {  AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import iconShop from '../../assets/iconshop.png';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={iconShop} alt="Commerce.js" height="25px" className={classes.image}/>
+                        e-Shop
+                    </Typography>
+                    <div className={classes.grow}/>
+                    <div className={classes.button}>
+                        <IconButton aria-label='Show cart items' color='inherit'>
+                            <Badge badgeContent={2} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            
+        </div>
+    )
+}
+
+export default Navbar
